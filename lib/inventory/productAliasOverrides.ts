61cf19e2ddfb8db42967ff1a3bd9d48c043dc6fb
Ryan_McNamara <rjmcnamara22@gmail.com>
2026-06-13 15:10:32 -0400

    Add aliases for bar items

--- lib/inventory/productAliasOverrides.ts
+++ lib/inventory/productAliasOverrides.ts
@@ -1,14 +1,46 @@
 export const productAliasOverrides: Record<string, string[]> = {
-  "12oz bottle hi life": [
+  "bud light": ["bud light", "budlight", "bud lite"],
+
+  budweiser: ["budweiser", "bud", "bud weiser"],
+
+  "coors light": ["coors", "coors lite", "coorslight"],
+
+  "dos equis": ["dos equis", "dos", "equis"],
+
+  "fh head hunter can": ["headhunter", "head hunter", "fat heads head hunter"],
+
+  "high life bottle": [
     "miller high life",
     "miller hi life",
     "high life",
     "hi life",
   ],
 
-  "hi life": ["miller high life", "miller hi life", "high life", "hi life"],
+  "high noon": ["high noon", "highnoon", "high noon seltzer"],
 
-  "bud light": ["bud", "budlight", "bud lite"],
+  "mich ultra": ["mich ultra", "michelob ultra", "mich"],
 
-  "coors light": ["coors", "coors lite", "coorslight"],
+  "miller lite": ["miller", "miller lite", "millerlite"],
+
+  modelo: ["modelo", "model", "modelo beer"],
+
+  narragansett: ["narragansett", "narragansett beer"],
+
+  pabst: ["pabst", "pbr", "pabst blue ribbon"],
+
+  "strongbow cider": ["strongbow"],
+
+  "sun cruiser": ["sun cruiser", "suncruiser", "sun cruiser tea"],
+
+  "sunny d vodka seltzer": [
+    "sunny d vodka seltzer",
+    "sunny d",
+    "sunny d seltzer",
+  ],
+
+  truth: ["truth", "rhinegeist truth", "truth ipa"],
+
+  "twisted tea": ["twisted tea", "twea", "twistedtea"],
+
+  "white claw": ["white claw", "whiteclaw", "white claw seltzer"],
 };
